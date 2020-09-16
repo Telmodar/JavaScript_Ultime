1a470b3f22d7f4d49919d17c4ae4a5fad26262eb
Telmodar <christophre09@gmail.com>
2020-09-16 21:41:45 +0200

    Primary Update
    Primary Update

---
+++ exemple2.js
@@ -0,0 +1,17 @@
+let prenom = "Christophe";
+let nom = "Santamaria";
+
+console.log(prenom);
+console.log(nom);
+
+
+prenom = "Tom";
+nom = "Pouce";
+let age = 18;
+
+
+console.log("Bonjour " + prenom + ' ' + nom + " !");
+console.log(age);
+
+
+
